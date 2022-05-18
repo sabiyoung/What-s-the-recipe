0f4d754f6f82d6829e8502695f5ca45b5c75f84a
saba <sabera@alphaworks.tech>
2022-05-17 18:36:37 -0700

    ref(schema): change foodIngredients type from array to string

--- server/schemas/recipe.schema.js
+++ server/schemas/recipe.schema.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 const recipeSchema = new mongoose.Schema({
     foodName:{type:String,required:true},
     foodImage:{type:String,required:true},
-    foodIngredients:[{type:Array,required:true}]
+    foodIngredients:[{type:String,required:true}]
 });
 export const RecipeModel = mongoose.model('Recipe', recipeSchema )

ce4f5a37bfbb8ddcdccb5278f7474752d993b097
saba <sabera@alphaworks.tech>
2022-05-23 11:45:10 -0700

    display recipes in index html

--- client/js/main.js
+++ client/js/main.js
@@ -1,24 +1,35 @@
 /* Start-Tsion */
-let preveiwContainer = document.querySelector(".products-preview");
-let previewBox = preveiwContainer.querySelectorAll(".preview");
+// let preveiwContainer = document.querySelector(".products-preview");
+// let previewBox = preveiwContainer.querySelectorAll(".preview");
 
-document.querySelectorAll(".products-container .product").forEach((product) => {
-	product.onclick = () => {
-		preveiwContainer.style.display = "flex";
-		let name = product.getAttribute("data-name");
-		previewBox.forEach((preview) => {
-			let target = preview.getAttribute("data-target");
-			if (name == target) {
-				preview.classList.add("active");
-			}
-		});
-	};
-});
+// document.querySelectorAll(".products-container .product").forEach((product) => {
+// 	product.onclick = () => {
+// 		preveiwContainer.style.display = "flex";
+// 		let name = product.getAttribute("data-name");
+// 		previewBox.forEach((preview) => {
+// 			let target = preview.getAttribute("data-target");
+// 			if (name == target) {
+// 				preview.classList.add("active");
+// 			}
+// 		});
+// 	};
+// });
 
-previewBox.forEach((close) => {
-	close.querySelector(".fa-times").onclick = () => {
-		close.classList.remove("active");
-		preveiwContainer.style.display = "none";
-	};
-});
+// previewBox.forEach((close) => {
+// 	close.querySelector(".fa-times").onclick = () => {
+// 		close.classList.remove("active");
+// 		preveiwContainer.style.display = "none";
+// 	};
+// });
 /* end-Tsion */
+import { apiService as api } from "../service/api.service.js";
+api.get('recipe').then(recipes => {
+	recipes.data.forEach(recipe => {
+		const li = document.createElement('li');
+		li.innerHTML = `
+     <img src=${recipe.foodImage} />
+	 <h3> ${recipe.foodName} </h3>
+		`;
+		document.body.querySelector('#product').appendChild(li)
+});
+})

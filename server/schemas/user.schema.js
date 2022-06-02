376ad0aa2d9b2559a860ff9b508992d6de1d10a5
saba <sabera@alphaworks.tech>
2022-06-02 10:53:02 -0700

    feat(schema): create user chema

---
+++ server/schemas/user.schema.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+});
+
+export const UserModel = mongoose.model('User',userSchema)
